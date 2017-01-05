222f8d007d48340cb1b405a1725ff8ae7b0fab6a
Vinayak Kaniyarakkal <vinayak.k@brisatech.com>
2017-01-05 15:45:18 +0530

    Initial commit

---
+++ aliases.sh
@@ -0,0 +1,24 @@
+############################################################################################
+#                                                Main Alias
+#                                            -------------------
+alias kbv='xmodmap ~/.bashrc.d/.xmodmap-vinayak'
+alias kbn='xmodmap ~/.bashrc.d/.xmodmap-normal'
+alias rf='rm -rf'
+alias e='emacs -nw'
+############################################################################################
+#                                                Git Alias
+#                                               -----------
+alias 9='git'
+alias 9a='git add'
+alias 9c='git checkout'
+alias 9cm='git commit -m'
+alias 9l='clear; echo "Git Log"; git log --pretty=oneline'
+alias 9s='git status'
+alias 9k='gitk --all &'
+alias 9d='git diff'
+############################################################################################
+#                                              Python Django Alias
+#                                             ---------------------
+alias p='ipython'
+alias m='python manage.py'
+############################################################################################
